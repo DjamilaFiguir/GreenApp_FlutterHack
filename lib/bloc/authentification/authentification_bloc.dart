32aced833f64c82081b44c0f25d1c1115907baec
DjamilaFiguir <djamila.figuir@gmail.com>
2020-06-27 16:30:46 +0200

    bloc

---
+++ lib/bloc/authentification/authentification_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'authentification_event.dart';
+part 'authentification_state.dart';
+
+class AuthentificationBloc
+    extends Bloc<AuthentificationEvent, AuthentificationState> {
+  @override
+  AuthentificationState get initialState => AuthentificationInitial();
+
+  @override
+  Stream<AuthentificationState> mapEventToState(
+    AuthentificationEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
